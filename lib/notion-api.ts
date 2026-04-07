4c349f9707c5280d47d004dcc8d5231de07c65e2
wuyw11 <96730410+wuyw11@users.noreply.github.com>
2026-04-07 11:54:59 +0800

    Update notion-api.ts

--- lib/notion-api.ts
+++ lib/notion-api.ts
@@ -1,6 +1,6 @@
 import { NotionAPI } from 'notion-client'
 
 export const notion = new NotionAPI({
-  authToken: process.env.NOTION_TOKEN,
+  token: process.env.NOTION_TOKEN,
   apiBaseUrl: process.env.NOTION_API_BASE_URL
 })
